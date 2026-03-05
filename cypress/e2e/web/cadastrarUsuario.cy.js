da86e0b4cdde5bc15dd94f4928339ace66dbae80
Tonia Dias <tdsouzza@gmail.com>
2026-03-05 11:36:57 -0300

    test: melhoria no fluxo de cadastro e login nos testes de web

--- cypress/e2e/web/cadastrarUsuario.cy.js
+++ cypress/e2e/web/cadastrarUsuario.cy.js
@@ -2,33 +2,22 @@ import { gerarUsuario } from '../../support/factories/usuarioFactory.js';
 
 describe('Cadastro Usuario', () => {
   before(() => {
-    cy.fixture('login').then((usuario) => {
-      cy.visit('/login');
-      cy.get('[data-testid="email"]').type(usuario.email);
-      cy.get('[data-testid="senha"]').type(usuario.senha);
-
-      cy.get('[data-testid="entrar"]').click();
-
-      cy.url().should('include', '/admin/home');
-      cy.contains('h1', 'Bem Vindo').should('be.visible');
-    });
+    cy.visit('/login');
   });
 
-  it('Deve permitir cadastro de novo usuário sem ser administrador', () => {
+  it('Deve permitir cadastro de novo usuário sem ser administrador - login', () => {
     const usuario = gerarUsuario();
+    cy.get('[data-testid="cadastrar"]').click();
 
-    cy.get('[data-testid="cadastrarUsuarios"]').click();
-
-    cy.url().should('include', '/admin/cadastrarusuarios');
-    cy.contains('h1', 'Cadastro de usuários').should('be.visible');
+    cy.url().should('include', '/cadastrarusuarios');
+    cy.contains('.font-robot', 'Cadastro').should('be.visible');
 
     cy.get('[data-testid="nome"]').type(usuario.nome);
     cy.get('[data-testid="email"]').type(usuario.email);
-    cy.get('[data-testid="password"]').type(usuario.senha);
+    cy.get('[data-testid="password"]').type(usuario.password);
     cy.get('[data-testid="checkbox"]').should('not.be.checked');
-    cy.get('[data-testid="cadastrarUsuario"]').click();
+    cy.get('[data-testid="cadastrar"]').click();
 
-    cy.url().should('include', '/admin/listarusuarios');
-    cy.contains('h1', 'Lista dos usuários').should('be.visible');
+    cy.get('.alert-link').should('have.text', 'Cadastro realizado com sucesso');
   });
 });
